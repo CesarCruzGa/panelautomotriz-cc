3ef99ed1c82cab9c10a0982b60b31a668973d562
CesarCruzGa <43935896+CesarCruzGa@users.noreply.github.com>
2019-09-12 12:46:37 -0500

    Archivo_Quehay

---
+++ public/prueba.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	$(".header").load("header.html");
+	$(".contenido").load("./contenido.html");
+	$(".footer").load("./footer.html");
+
+
+	
+	$(".navegar").click(function(event){
+		event.preventDefault();
+		let pagina = $(this).data('pagina');
+		$('.contenido').load(pagina);
+
+	});
+
+
+/*
+	function llamarcontenido(dato){
+		var c = dato.dataset.cont;
+		console.log(c);
+	
+	alert("Que hay !!!!!")
+	$(".contenido").load('./'+c+'.html');
+	}
+	*/
+});
